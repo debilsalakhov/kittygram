074b74393e01efd16ec3b9d686f3e8c9169c7e5c
debilsalakhov <bigmoyka@gmail.com>
2024-11-23 10:17:05 +0300

    add token

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,14 +1,17 @@
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 from django.urls import path, include
 
-from cats.views import CatViewSet, OwnerViewSet
+from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
 router.register('owners', OwnerViewSet)
+router.register(r'mycats', LightCatViewSet, basename='CreateRetrieveCats')
 
 urlpatterns = [
    path('', include(router.urls)),
+   path('api-token-auth/', views.obtain_auth_token),
 ]
